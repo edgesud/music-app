bb7362655c6ae1cdb3c0f064e9df7eae01d261d4
edgesud <73988135+edgesud@users.noreply.github.com>
2021-07-21 11:24:34 +0530

    Add files via upload

---
+++ model/data.js
@@ -0,0 +1,39 @@
+const Songs = [
+    {
+        title: "Ganesh Aarti",
+        artist:"Anuradha Paudwal",
+        image: require("../assets/image/ganesh.jpg"),
+        id: "1",
+    },
+    {
+        title: "Hey Govind",
+        artist:"Jagjit Singh",
+        image: require("../assets/image/jagjit.jpg"),
+        id: "2",
+    },
+    {
+        title: "Arey Dwaarpalo",
+        artist:"Lakhbir singh Lakkha",
+        image: require("../assets/image/krishna1.jpg"),
+        id: "3",
+    },
+    {
+        title: "Maakhan Khaa Gaye Makhan Chor",
+        artist:"Mridul Shastri",
+        image: require("../assets/image/krishna2.jpg"),
+        id: "4",
+    },
+    {
+        title: "Sri Ramchandra Kripalu Bhajman",
+        artist:"Bhimsen Joshi",
+        image: require("../assets/image/Ram.jpg"),
+        id: "5",
+    },
+    {
+        title: "Hanuman Ashtak",
+        artist:"Hari Om Sharan",
+        image: require("../assets/image/hanuman2.jpg"),
+        id: "6",
+    },
+]
+export default Songs;
